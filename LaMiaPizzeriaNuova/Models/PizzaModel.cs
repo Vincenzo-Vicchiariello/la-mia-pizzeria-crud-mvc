a3156af6f14d108ebe4de191a0f791269ba574bd
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-05-17 14:55:32 +0200

    creato model pizzamodel, creato controller pizzacontroller e impostato controller di default in program.cs -neverGonnaGiveYouUp

---
+++ LaMiaPizzeriaNuova/Models/PizzaModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaMiaPizzeriaNuova.Models
+
+{
+    public class PizzaModel
+    {
+        int Id { get; set; }
+        [MaxLength(40)]
+        public string Name { get; set; }
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+        [MaxLength(300)]
+        public string imgSource { get; set; }
+        public float Price { get; set;}
+    }
+}
