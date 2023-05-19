8cd91e0f45035831abf248ddc128857aeb2e3e98
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-05-19 16:36:37 +0200

    Aggiunta funzione per cancellare pizze.

--- LaMiaPizzeriaNuova/Controllers/PizzaController.cs
+++ LaMiaPizzeriaNuova/Controllers/PizzaController.cs
@@ -22,25 +22,94 @@ namespace LaMiaPizzeriaNuova.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(PizzaModel data)
+        public IActionResult CreatePizza(PizzaModel data)
         {
             if (!ModelState.IsValid)
             {
-                return View("Create", data);
+                return View("CreatePizza", data);
             }
             using (PizzaContext context = new PizzaContext())
             {
                 PizzaModel pizzaToCreate = new PizzaModel();
                 pizzaToCreate.Name = data.Name;
                 pizzaToCreate.Description = data.Description;
-                context.PizzaContext.Add(pizzaToCreate);
+                pizzaToCreate.ImgSource = data.ImgSource;
+                pizzaToCreate.Price = data.Price;
+                context.Pizze.Add(pizzaToCreate);
                 context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
         }
+        [HttpGet]
+        public IActionResult ModifyPizza(int Id)
+        {
+            using (PizzaContext ctx = new PizzaContext())
+            {
+                PizzaModel? pizzaToModify = ctx.Pizze.Where(pizza => pizza.Id == Id).FirstOrDefault();
+                if (pizzaToModify != null)
+                {
+                    return View(pizzaToModify);
+                }
+            }
+            return NotFound("Questa pizza non esiste..");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryTokenAttribute]
+        public IActionResult ModifyPizza(int Id, PizzaModel ModifiedPizza)
+        {
+
+            if (ModelState.IsValid)
+            {
+                using (PizzaContext context = new PizzaContext())
+                {
+                    PizzaModel? pizzaToModify = context.Pizze.Where(pizza => pizza.Id == Id).FirstOrDefault();
+                    if (pizzaToModify != null)
+                    {
+                        pizzaToModify.Name = ModifiedPizza.Name;
+                        pizzaToModify.Description = ModifiedPizza.Description;
+                        pizzaToModify.ImgSource = ModifiedPizza.ImgSource;
+                        pizzaToModify.Price = ModifiedPizza.Price;
+                        context.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return NotFound("Questa pizza non esiste..");
+                    }
+                }
+
+            }
+            else return View(ModifiedPizza);
 
 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryTokenAttribute]
+        public ActionResult DestroyPizza(int Id)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+
+                PizzaModel? pizzaToDestroy = context.Pizze.Where(pizza => pizza.Id == Id).FirstOrDefault();
+
+                if (pizzaToDestroy != null)
+                {
+                    context.Remove(pizzaToDestroy);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("Non c'è una pizza da cancellare con questo ID.");
+                }
+            }
+
+
+        }
 
     }
+
 }
--- LaMiaPizzeriaNuova/Models/PizzaModel.cs
+++ LaMiaPizzeriaNuova/Models/PizzaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using LaMiaPizzeriaNuova.Models.CustomValidationAttributes;
 
 namespace LaMiaPizzeriaNuova.Models
 
@@ -7,13 +8,25 @@ namespace LaMiaPizzeriaNuova.Models
     public class PizzaModel
     {
         public int Id { get; set; }
-        [MaxLength(40)]
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(40, ErrorMessage = "Il nome non può avere più di 40 caratteri")]
         public string Name { get; set; }
         [Column(TypeName = "text")]
+        [Required(ErrorMessage = "Il campo è obbligatorio")] 
         public string Description { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Url(ErrorMessage = "L'URL inserito non è un url valido!")]
         [MaxLength(300)]
         public string ImgSource { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [MoreThanZero(ErrorMessage = "Il prezzo deve essere maggiore di 0!")]
         public float Price { get; set;}
+
+
+        public PizzaModel()
+        {
+        }
+
 
         public PizzaModel(string name, string description, string imgSource, float price)
         {
